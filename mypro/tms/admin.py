d1610fd915d663e7fc7b35063418b5c3aa104996
irelise54 <irelise54@gmail.com>
2026-04-01 16:27:06 +0200

    add Organization model and configure admin site branding

--- mypro/tms/admin.py
+++ mypro/tms/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Teacher, Users
+from .models import Teacher, Users, Subject
+
+admin.site.site_header = "Teacher Management System Admin"
+admin.site.site_title = "TMS Admin Portal"
+admin.site.index_title = "Welcome to the TMS Portal"
 
 class TeacherAdmin(admin.ModelAdmin):
     list_display = ("tname", "display_subjects", "qualification", "contact", "status")
@@ -14,3 +18,9 @@ class UsersAdmin(admin.ModelAdmin):
     list_display = ("names", "username", "password")
 
 admin.site.register(Users, UsersAdmin)
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+admin.site.register(Subject, SubjectAdmin)
+
--- mypro/tms/apps.py
+++ mypro/tms/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class TmsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "tms"
+    verbose_name = "Teacher Management System"
